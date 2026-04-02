a3c99d5259b020cd87f7e2c38dc818893b77bf02
Arpit Mishra <arpitmishra172006@gmail.com>
2026-04-02 22:35:45 +0530

    find the sum of first n numbers

---
+++ java_progress-repo/Phase-1-Basics/Sum_Of_Numbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Sum_Of_Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+}
